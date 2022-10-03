fd9838b95db15358116e424ff3b94e889ce31903
Fatao <abdulfataoabdulrahman@gmail.com>
2022-10-03 04:39:21 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,27 +1,44 @@
-#include <stdlib.h> 
+#include <stdio.h> 
+ #include <stdlib.h> 
+ #include <string.h> 
  #include <ctype.h> 
+ #include "main.h" 
+  
  /** 
-  * main - main funct 
-  * @argc: param count 
-  * @argv: param poiter 
-  * Return: int 
+  * main - Program that takes in all integer arguments and returns the sum 
+  * @argc: Number of command line arguments 
+  * @argv: Array name 
+  * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise 
   */ 
+  
  int main(int argc, char *argv[]) 
  { 
-         int i, j, sum = 0; 
+         int i, j, length, sum; 
+         char *ptr; 
   
-         for (i = 1; i < argc; i++) 
+         if (argc < 2) 
+         printf("0\n"); 
+         else 
          { 
-                 for (j = 0; argv[i][j] != '\0'; j++) 
+                 sum = 0; 
+                 for (i = 1; i < argc; i++) 
                  { 
-                         if (!isdigit(argv[i][j])) 
+                         ptr = argv[i]; 
+                         length = strlen(ptr); 
+  
+                         for (j = 0; j < length; j++) 
                          { 
-                                 printf("Error\n"); 
-                                 return (1); 
+                                 if (isdigit(*(ptr + j)) == 0) 
+                                 { 
+                                         printf("Error\n"); 
+                                         return (1); 
+                                 } 
                          } 
+  
+                         sum += atoi(argv[i]); 
                  } 
-                 sum += atoi(argv[i]); 
+  
+                 printf("%d\n", sum); 
          } 
-         printf("%d\n", sum); 
          return (0); 
  }
